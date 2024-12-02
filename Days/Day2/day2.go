c35bb3012b04bf3fb8270b8b37507fd0b6a0a6e7
liaml17 <liam.louw17@gmail.com>
2024-12-02 12:08:40 +0200

    Day 2 with stolen code from thisRedH/AdventOfCode

--- Days/Day2/day2.go
+++ Days/Day2/day2.go
@@ -3,6 +3,7 @@ package day2
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -20,25 +21,70 @@ func Run() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	total := 0
+	lineCount := 0
+
+	safeSequence := 0
+	safeSequenceDampened := 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.Split(line, " ")
 		array := toIntArray(parts)
 
-		safe := false
-		if array[0] < array[1] {
-			safe = checkIncreasing(array)
-		} else if array[0] > array[1] {
-			safe = checkDecreasing(array)
+		if IsSafeSequence(array) {
+			safeSequence++
 		}
 
-		if safe {
-			total++
+		if IsSafeSequenceDampened(array) {
+			safeSequenceDampened++
+		}
+
+		lineCount++
+	}
+
+	fmt.Println("Total:", safeSequence)
+	fmt.Println("Total Part Two:", safeSequenceDampened)
+}
+
+func RemoveIdx(s []int, i int) []int {
+	r := make([]int, 0)
+	r = append(r, s[:i]...)
+	return append(r, s[i+1:]...)
+}
+
+func IsSafeSequence(nums []int) bool {
+	cmp := func(a, b int) bool { return a < b }
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i]-nums[i+1] == 0 {
+			continue
+		}
+		if nums[i]-nums[i+1] > 0 {
+			cmp = func(a, b int) bool { return a > b }
+		}
+		break
+	}
+
+	for i := 1; i < len(nums); i++ {
+		diff := math.Abs(float64(nums[i] - nums[i-1]))
+
+		if diff < 1 || diff > 3 || !cmp(nums[i-1], nums[i]) {
+			return false
 		}
 	}
 
-	fmt.Println("Total:", total)
+	return true
+}
+
+func IsSafeSequenceDampened(nums []int) bool {
+	if IsSafeSequence(nums) {
+		return true
+	}
+	for i, _ := range nums {
+		if IsSafeSequence(RemoveIdx(nums, i)) {
+			return true
+		}
+	}
+	return false
 }
 
 func toIntArray(parts []string) []int {
@@ -50,30 +96,4 @@ func toIntArray(parts []string) []int {
 	}
 
 	return intArray
-}
-
-func checkIncreasing(array []int) bool {
-	previous := array[0]
-	for i := 1; i < len(array); i++ {
-		diff := array[i] - previous
-		if diff <= 0 || diff >= 4 {
-			return false
-		}
-
-		previous = array[i]
-	}
-	return true
-}
-
-func checkDecreasing(array []int) bool {
-	previous := array[0]
-	for i := 1; i < len(array); i++ {
-		diff := previous - array[i]
-		if diff <= 0 || diff >= 4 {
-			return false
-		}
-
-		previous = array[i]
-	}
-	return true
 }

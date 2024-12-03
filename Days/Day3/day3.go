e622c7d357908fc5b03250979b14bbc14e1aabc2
liaml17 <liam.louw17@gmail.com>
2024-12-03 13:25:00 +0200

    Day 3 part 2

--- Days/Day3/day3.go
+++ Days/Day3/day3.go
@@ -17,6 +17,14 @@ func Run() {
 	buf, err := ioutil.ReadFile("days/day3/input.txt")
 	check(err)
 
+	total := partOne(buf)
+	total2 := partTwo(buf)
+
+	fmt.Println("Total part 1:", total)
+	fmt.Println("Total part 2:", total2)
+}
+
+func partOne(buf []byte) int {
 	// Must match mul(X,Y) where X & Y is any 3 digit number
 	regex, _ := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)`)
 
@@ -35,6 +43,42 @@ func Run() {
 		total += multiplied
 	}
 
-	fmt.Println("Total:", total)
+	return total
 
+}
+
+func partTwo(buf []byte) int {
+	total := 0
+	do := true
+
+	// Must match mul(X,Y) where X & Y is any 3 digit number OR do() OR don't()
+	regex, _ := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)`)
+
+	matches := regex.FindAllString(string(buf), -1)
+	for _, element := range matches {
+		if element == "do()" {
+			do = true
+			continue
+		}
+
+		if element == "don't()" {
+			do = false
+			continue
+		}
+
+		if do {
+			regex, _ := regexp.Compile(`\d{1,3}`)
+			nums := regex.FindAllString(element, -1)
+
+			num1, _ := strconv.Atoi(nums[0])
+			num2, _ := strconv.Atoi(nums[1])
+			multiplied := num1 * num2
+
+			// fmt.Printf("%s = %d * %d = %d\n", element, num1, num2, multiplied)
+			total += multiplied
+		}
+
+	}
+
+	return total
 }

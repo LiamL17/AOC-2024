1854e0550a8e577c8645fc192ad3663c641d98c0
liaml17 <liam.louw17@gmail.com>
2024-12-03 13:17:32 +0200

    Day 3 part 1

--- Days/Day3/day3.go
+++ Days/Day3/day3.go
@@ -1,7 +1,40 @@
 package day3
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func Run() {
-	fmt.Println("Running Day 3!")
+	buf, err := ioutil.ReadFile("days/day3/input.txt")
+	check(err)
+
+	// Must match mul(X,Y) where X & Y is any 3 digit number
+	regex, _ := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)`)
+
+	matches := regex.FindAllString(string(buf), -1)
+	total := 0
+
+	for _, element := range matches {
+		regex, _ := regexp.Compile(`\d{1,3}`)
+		nums := regex.FindAllString(element, -1)
+
+		num1, _ := strconv.Atoi(nums[0])
+		num2, _ := strconv.Atoi(nums[1])
+		multiplied := num1 * num2
+
+		// fmt.Printf("%s = %d * %d = %d\n", element, num1, num2, multiplied)
+		total += multiplied
+	}
+
+	fmt.Println("Total:", total)
+
 }

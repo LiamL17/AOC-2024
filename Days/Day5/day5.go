7d2f301344fb1478028772263554282e5a0087be
Liam <liam.louw17@gmail.com>
2024-12-05 23:04:16 +0200

    Start day 5

--- Days/Day5/day5.go
+++ Days/Day5/day5.go
@@ -1,7 +1,36 @@
 package day5
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func Run() {
-    fmt.Println("Running Day 5!")
+	file, err := os.Open("days/day5/test.txt")
+	check(err)
+	defer file.Close()
+
+	// rules := make(map[int][]int)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 5 {
+			fmt.Println(line)
+			num1, _ := strconv.Atoi(strings.Split(line, "")[0:1])
+			num2, _ := strconv.Atoi(strings.Split(line, "")[3:4])
+			fmt.Println(num1, ":", num2)
+		} else {
+
+		}
+	}
 }

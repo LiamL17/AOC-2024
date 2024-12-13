496e4e4fef32e20dfb4de0ad641242fc17f4c5e6
Liam <liam.louw17@gmail.com>
2024-12-13 23:29:08 +0200

    Day 11 part 1, part 2 not efficient enough

--- Days/Day10/day10.go
+++ Days/Day10/day10.go
@@ -3,5 +3,5 @@ package day10
 import "fmt"
 
 func Run() {
-    fmt.Println("Running Day 10!")
+	fmt.Println("Running Day 10!")
 }
--- Days/Day11/day11.go
+++ Days/Day11/day11.go
@@ -1,7 +1,71 @@
 package day11
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func Run() {
-    fmt.Println("Running Day 11!")
+	buf, err := ioutil.ReadFile("days/day11/input.txt")
+	check(err)
+
+	bufferString := string(buf)
+	stones := strings.Split(bufferString, " ")
+
+	// fmt.Println("\nStones after", blinks, "blinks:", splited)
+
+	fmt.Println("Number of stones Part 1 (25 blinks):", len(blink(0, 25, stones)))
+	// fmt.Println("Number of stones Part 2 (75 blinks):", len(blink(0, 75, stones)))
+
+}
+
+func blink(blinks int, maxBlinks int, array []string) []string {
+	// fmt.Println("Bink no.", blinks, "Array", array)
+	if blinks == maxBlinks {
+		return array
+	}
+
+	var newArray []string
+
+	for _, stone := range array {
+
+		if len(stone)%2 == 0 {
+			// split into 2 stones
+			left, right := split(stone)
+			leftInt, _ := strconv.Atoi(left)
+			rightInt, _ := strconv.Atoi(right)
+
+			// array = append(array[:index], append([]string{left, right}, array[index+1:]...)...)
+			newArray = append(newArray, strconv.Itoa(leftInt))
+			newArray = append(newArray, strconv.Itoa(rightInt))
+
+		} else if stone == "0" {
+			// replace stone with '1'
+			newArray = append(newArray, "1")
+			// array[index] = "1"
+		} else {
+			// stone * 1024
+			stoneInt, _ := strconv.Atoi(stone)
+			// array[index] = strconv.Itoa(stoneInt * 2024)
+			newArray = append(newArray, strconv.Itoa(stoneInt*2024))
+		}
+
+	}
+
+	return blink(blinks+1, maxBlinks, newArray)
+}
+
+func split(stone string) (string, string) {
+	len := len(stone)
+	left := stone[:len/2]
+	right := stone[len/2:]
+	return left, right
 }
